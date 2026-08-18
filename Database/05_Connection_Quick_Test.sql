c46cf0a9c3ea080192dd1157368d3b369a7fe5bb
KirtiBabco <kirti@babcofoods.com>
2026-08-18 17:15:47 +0530

    First

--- Database/05_Connection_Quick_Test.sql
+++
@@ -1,8 +0,0 @@
-USE [Babco];
-GO
-SELECT DB_NAME() AS CurrentDatabase;
-SELECT COUNT(*) AS UOS_Order_Count FROM dbo.UOS_Order;
-SELECT COUNT(*) AS UOS_OrderDetail_Count FROM dbo.UOS_OrderDetail;
-SELECT COUNT(*) AS Item_Count FROM dbo.Item;
-SELECT TOP (10) * FROM dbo.UOS_Order ORDER BY 1 DESC;
-GO
